7c82f34019894848d24c5b8dc7192e01b02b78e5
MariaPeres <64583990+MariaSilvaPeres@users.noreply.github.com>
2021-02-05 11:55:56 +0000

    adicionei as datas às promoções e promocoesDAO

--- src/main/java/standard_value_object/Promocao.java
+++ src/main/java/standard_value_object/Promocao.java
@@ -1,5 +1,8 @@
 package standard_value_object;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
 public class Promocao {
 
 	private int id;
@@ -7,12 +10,18 @@ public class Promocao {
 	private String descricao;
 	private boolean ativa;
 	
-	public Promocao(int id, String nome, String descricao, boolean ativa) {
+	private Date data_inicio; 
+	
+	private Date data_fim;
+	
+	public Promocao(int id, String nome, String descricao, boolean ativa, Date data_inicio, Date data_fim) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
 		this.ativa = ativa;
+		this.data_inicio = data_inicio;
+		this.data_fim = data_fim; 
 	}
 
 	public Promocao(String nome, String descricao, boolean ativa) {
@@ -53,11 +62,43 @@ public class Promocao {
 	public void setAtiva(boolean ativa) {
 		this.ativa = ativa;
 	}
+	
+
+	/**
+	 * @return the data_inicio
+	 */
+	public Date getData_inicio() {
+		return data_inicio;
+	}
+
+	/**
+	 * @param data_inicio the data_inicio to set
+	 */
+	public void setData_inicio(Date data_inicio) {
+		this.data_inicio = data_inicio;
+	}
+
+	/**
+	 * @return the data_fim
+	 */
+	public Date getData_fim() {
+		return data_fim;
+	}
+
+	/**
+	 * @param data_fim the data_fim to set
+	 */
+	public void setData_fim(Date data_fim) {
+		this.data_fim = data_fim;
+	}
 
 	@Override
 	public String toString() {
-		return "Promocao [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", ativa=" + ativa + "]";
+		return "Promocao [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", ativa="
+				+ ativa + ", data_inicio=" + data_inicio + ", data_fim=" + data_fim + "]";
 	}
+
+	
 	
 	
 }
