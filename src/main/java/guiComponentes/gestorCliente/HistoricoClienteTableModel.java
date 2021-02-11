627ee0ed4bd3b401e7eea95f36c98b0dd2eea10d
robinginja <robinlginja@gmail.com>
2021-02-11 17:49:39 +0000

    ad

--- src/main/java/guiComponentes/gestorCliente/HistoricoClienteTableModel.java
+++ src/main/java/guiComponentes/gestorCliente/HistoricoClienteTableModel.java
@@ -15,7 +15,7 @@ public class HistoricoClienteTableModel extends AbstractTableModel {
 	private static final int FUNCIONARIO_COL = 2;
 	private static final int ID_COL = 3;
 
-
+int test;
 	private String[] columnNames = { "Data", "Registo", "Funcionario", "ID Admin" };
 
 	private List<HistoricoCliente> historicoLista;
