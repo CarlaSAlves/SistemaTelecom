e14657b6a141af2b38cdb9a8c0afeab09655b62a
MariaPeres <64583990+MariaSilvaPeres@users.noreply.github.com>
2021-02-23 11:31:51 +0000

    operador comentarios ok

--- src/main/java/guiComponentes/admin_gestorOperador/HistoricoOperadorDialog.java
+++ src/main/java/guiComponentes/admin_gestorOperador/HistoricoOperadorDialog.java
@@ -25,28 +25,14 @@ import standard_value_object.Funcionario;
 
 public class HistoricoOperadorDialog extends JDialog {
 
-	
+
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
 	private JTable table;
 	private JLabel operadorRegistoLabel;
 
-	
-
-	public void preencherTable(Funcionario funcionario,List<HistoricoOperador> historicoFuncionario) {
-		operadorRegistoLabel.setText(funcionario.getNome());
-		
-		HistoricoOperadorTableModel model = new HistoricoOperadorTableModel(historicoFuncionario); 
-		
-		table.setModel(model);
-		
-		TableCellRenderer tableCellRenderer = new DateTimeCellRenderer();
-		table.getColumnModel().getColumn(HistoricoOperadorTableModel.DATA_COL).setCellRenderer(tableCellRenderer);		
-		
-	}
-	
 	public HistoricoOperadorDialog() {
-		setBounds(100, 100, 450, 300);
+
 		setTitle("Historico de Registos");
 		setModal(true);
 		setBounds(100, 100, 651, 300);
@@ -55,60 +41,81 @@ public class HistoricoOperadorDialog extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		setResizable(false);
 		contentPanel.setLayout(new BorderLayout(0, 0));
-		{
-			JPanel panel = new JPanel();
-			contentPanel.add(panel, BorderLayout.NORTH);
-			panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-			{
-				JLabel lblHistoricoPara = new JLabel("Historico do Operador :");
-				panel.add(lblHistoricoPara);
-			}
-			{
-				operadorRegistoLabel = new JLabel("");
-				panel.add(operadorRegistoLabel);
-			}
-		}
-		{
-			JScrollPane scrollPane = new JScrollPane();
-			contentPanel.add(scrollPane, BorderLayout.CENTER);
-			{
-				table = new JTable();
-				scrollPane.setViewportView(table);
-			}
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("Sair");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						setVisible(false);
-						dispose();
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-		}		
-	}
-	
-	private final class DateTimeCellRenderer extends DefaultTableCellRenderer {
+
+		// Painel de histórico
+		JPanel panel = new JPanel();
+		contentPanel.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+
+		JLabel lblHistoricoPara = new JLabel("Historico do Operador :");
+		panel.add(lblHistoricoPara);
+
+		operadorRegistoLabel = new JLabel("");
+		panel.add(operadorRegistoLabel);
+
+		// Tabela e Scrollpane de histórico 
 		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPanel.add(scrollPane, BorderLayout.CENTER);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+
+		// Painel de rodapé - botão sair 
+		
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+		JButton okButton = new JButton("Sair");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+
+
+	}
+
+	/**
+	 * Método que preenche a tabela com o registos
+	 * @param funcionario
+	 * @param historicoFuncionario
+	 */
+	public void preencherTable(Funcionario funcionario,List<HistoricoOperador> historicoFuncionario) {
+		operadorRegistoLabel.setText(funcionario.getNome());
+
+		HistoricoOperadorTableModel model = new HistoricoOperadorTableModel(historicoFuncionario); 
+
+		table.setModel(model);
+
+		TableCellRenderer tableCellRenderer = new DateTimeCellRenderer();
+		table.getColumnModel().getColumn(HistoricoOperadorTableModel.DATA_COL).setCellRenderer(tableCellRenderer);		
+
+	}
+
+	/**
+	 * Renderer da tabela
+	 */
+	private final class DateTimeCellRenderer extends DefaultTableCellRenderer {
+
 		private static final long serialVersionUID = 5466469925138317415L;
 		SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
 		public Component getTableCellRendererComponent(JTable table,
-		        Object value, boolean isSelected, boolean hasFocus,
-		        int row, int column) {
-		    if( value instanceof Date) {
-		        value = f.format(value);
-		    }
-		    return super.getTableCellRendererComponent(table, value, isSelected,
-		            hasFocus, row, column);
+				Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			if( value instanceof Date) {
+				value = f.format(value);
+			}
+			return super.getTableCellRendererComponent(table, value, isSelected,
+					hasFocus, row, column);
 		}
 	}
-	
-}
+
+} // end class
