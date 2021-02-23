72bc4144ef6f41e7d33e7f114fde104180ed4f0e
CarlaSAlves <carla.soeiro@gmail.com>
2021-02-23 10:48:23 +0000

    Qualquer coisa

--- src/main/java/guiComponentes/admin_gestorCliente/CriarClienteDialog.java
+++ src/main/java/guiComponentes/admin_gestorCliente/CriarClienteDialog.java
@@ -6,11 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
